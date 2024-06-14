853d5e17a7f575158c4667b0c7137c42ef2ea710
Grant Beaton <grant.beaton@stu.ocsb.ca>
2024-06-14 15:41:11 -0400

    Started

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+const prompt = require('prompt-sync')();
+document.getElementById("greet").innerHTML = categories()
+
+function categories(){
+    console.log("Here are your options of categories: \nAnimals \nFood \nJobs")
+    let cat = prompt("Which category would you like? ")
+    if (cat = "Animals"){
+
+    }
+    if (cat = "Food"){
+
+    }
+    if (cat = "Jobs"){
+
+    }
+}
