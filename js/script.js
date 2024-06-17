40addcb4f5a844db1c50cae149b8cd5d1743d1b8
Grant Beaton <grant.beaton@stu.ocsb.ca>
2024-06-17 13:41:32 -0400

    Trying

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 const prompt = require('prompt-sync')();
 
+
 let words = [
     "worst", "youth", "happy", "state", "billy", "alert", "after", "fifth", "chase", "hairy", "share", "still", "lease", "links", "youth",
 "today", "plain", "boost", "lease", "brand", "threw", "apple", "album", "broad", "noise", "breed", "maybe", "boost", "curve", "draft",
@@ -8,31 +9,25 @@ let words = [
 "about", "earth", "bases", "small","booth"
 ]
 
+document.getElementById("start_the_game").addEventListener("totalguesses", makeBoard());
+
 let totalguesses = 6;
 let guessesleft = totalguesses;
-let currentguess = [];
-let nextLetter = 0;
+let currentguess = prompt("First word");
 let correctguess = words[Math.floor(Math.random() * words.length)]
-console.log(correctguess)
 
 let board = document.getElementById("game-board");
-document.getElementById("start");
 
-function makeBoard() {
-
+function makeBoard(){
     for (let i = 0; i < totalguesses; i++) {
         let row = document.createElement("div")
-        row.className = "letter-row"
         
         for (let j = 0; j < 5; j++) {
             let box = document.createElement("div")
-            box.className = "letter-box"
             row.appendChild(box)
         }
 
         board.appendChild(row)
     }
 }
-makeBoard()
-
 
