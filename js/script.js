6aba3149f6d84439ae39db8f31a0a768a99bf63f
TonyShen7 <159183430+TonyShen7@users.noreply.github.com>
2024-06-16 22:28:14 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 'use strict';
 const prompt = require('prompt-sync')();
 document.getElementById("greet").innerHTML = categories()
-
+//Arrays for choosing words based on topics
+    const words = {
+    food: ["apple", "bread", "candy", "pizza", "salad", "steak", "sushi", "toast", "bacon", "lemon"],
+    job: ["nurse", "pilot", "actor", "judge", "coach", "clerk", "guard", "agent", "chief", "baker"],
+    animals: ["zebra", "tiger", "horse", "panda", "whale", "eagle", "koala", "snake", "shark", "otter"]
+};
 function categories(){
     console.log("Here are your options of categories: \nAnimals \nFood \nJobs")
     let cat = prompt("Which category would you like? ")
