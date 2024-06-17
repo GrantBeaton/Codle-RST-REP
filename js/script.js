2389c7487a4e7e26d21df9156db7fa17a3f56311
TonyShen7 <159183430+TonyShen7@users.noreply.github.com>
2024-06-16 22:46:35 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,17 +6,17 @@ document.getElementById("greet").innerHTML = categories()
     food: ["apple", "bread", "candy", "pizza", "salad", "steak", "sushi", "toast", "bacon", "lemon"],
     job: ["nurse", "pilot", "actor", "judge", "coach", "clerk", "guard", "agent", "chief", "baker"],
     animals: ["zebra", "tiger", "horse", "panda", "whale", "eagle", "koala", "snake", "shark", "otter"]
-};
-function categories(){
-    console.log("Here are your options of categories: \nAnimals \nFood \nJobs")
-    let cat = prompt("Which category would you like? ")
-    if (cat = "Animals"){
-
+        //ask for which categories
+function startGame() {
+    let selectedCategory;
+    while (true) {
+        selectedCategory = prompt("Here are your options of categories: Animals, Food, Jobs, Which category would you like?").toLowerCase();
+        if (selectedCategory == "animals" || selectedCategory == "food" || selectedCategory == "job") {
+            break;
+        } else {
+            alert("Please enter a 5-letter word.");
     }
-    if (cat = "Food"){
-
     }
-    if (cat = "Jobs"){
-
-    }
-}
+    //select random words from certain category
+    const wordList = words[selectedCategory];
+    targetWord = wordList[Math.floor(Math.random() * wordList.length)];
